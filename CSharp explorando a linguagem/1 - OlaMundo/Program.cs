d07707866582be236bea27dc1c055036d90716c2
Roger Pimenta <roger.pimenta@live.com>
2023-05-18 17:05:49 -0300

    primeiro programa

---
+++ CSharp explorando a linguagem/1 - OlaMundo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Programa {
+  static void Main(string[] args) {
+    Console.WriteLine("Olá, mundo!");
+    Console.WriteLine("Tecle enter para fechar.");
+    Console.ReadLine();
+  }
+}
