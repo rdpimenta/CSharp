c73cf2d4f1b1dc52d5cbe3edf66a87649cf25e91
Roger Pimenta <roger.pimenta@live.com>
2023-05-18 17:37:36 -0300

    variáveis numéricas

---
+++ CSharp explorando a linguagem/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa {
+  static void Main(string[] args) {
+    Console.WriteLine("Projeto 3 - Variáveis ponto flutuante.");
+
+    double salario;
+    salario = 3000.10;
+
+    salario = 3000;
+
+    double idade;
+    idade = 7.0 / 5;
+
+    Console.WriteLine(salario);
+    Console.WriteLine(idade);
+
+    Console.WriteLine("Tecle enter para fechar.");
+    Console.ReadLine();
+  }
+}
---
+++ CSharp explorando a linguagem/4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Programa {
+  static void Main(string[] args) {
+    Console.WriteLine("Projeto 4 - Conversões e outros tipos.");
+
+    double salario = 3000.15;
+    Console.WriteLine(salario);
+
+    int salarioInteiro = (int) salario;
+    Console.WriteLine(salarioInteiro);
+
+    long x = 2000000000000000000;
+    Console.WriteLine(x);
+
+    short y = 15000;
+    Console.WriteLine(y);
+
+    float altura = 1.62F;
+    Console.WriteLine(altura);
+
+    Console.WriteLine("Tecle enter para fechar.");
+    Console.ReadLine();
+  }
+}
